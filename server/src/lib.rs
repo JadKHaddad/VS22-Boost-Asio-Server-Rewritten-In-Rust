7410aaa6f38d619630071b53a1d8463c344cad3f
JadKHaddad <jadkhaddad@gmail.com>
2022-12-14 12:59:31 +0100

    update

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -10,8 +10,8 @@ use tokio::sync::mpsc::Sender;
 extern crate crossterm;
 use crossterm::{
     cursor,
-    style::Color,
     event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    style::Color,
     style::{Print, ResetColor, SetBackgroundColor, SetForegroundColor},
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
@@ -21,6 +21,7 @@ use std::io::{stdout, Write};
 pub struct Client {
     id: u32,
     position: Arc<RwLock<Position>>,
+    old_position: Arc<RwLock<Position>>,
     score: i32,
     sender: Option<Sender<String>>,
 }
@@ -29,7 +30,8 @@ impl Client {
     pub fn new(id: u32, position: Position, sender: Option<Sender<String>>) -> Self {
         Self {
             id,
-            position: Arc::new(RwLock::new(position)),
+            position: Arc::new(RwLock::new(position.clone())),
+            old_position: Arc::new(RwLock::new(position)),
             score: 0,
             sender,
         }
@@ -42,6 +44,10 @@ impl Client {
     pub fn set_position(&mut self, position: Position) {
         *self.position.write() = position;
     }
+
+    pub fn set_old_position(&mut self, position: Position) {
+        *self.old_position.write() = position;
+    }
 }
 
 pub struct Game {
@@ -50,12 +56,9 @@ pub struct Game {
     field: Field,
 }
 
-
 impl Drop for Game {
-    fn drop(&mut self) {
-    }
+    fn drop(&mut self) {}
 }
-
 
 impl Game {
     pub fn new(width: u16, height: u16, max_clients: u16) -> Self {
@@ -67,13 +70,11 @@ impl Game {
     }
 
     pub fn add_client(&self, client: Client) {
-        println!("adding client: {}", client.id);
         self.clients.write().push(client);
     }
 
     pub fn remove_client(&self, client: &Client) {
         self.clients.write().retain(|c| c.id != client.id);
-        println!("client removed: {}", client.id);
     }
 
     pub fn get_total_clients(&self) -> u32 {
@@ -101,6 +102,7 @@ impl Game {
 
     pub fn adjust_position(&self, client: &mut Client, direction: Direction) {
         let mut position = client.position.read().clone();
+        client.set_old_position(position.clone());
         match direction {
             Direction::Up => {
                 if position.y == 0 {
@@ -111,7 +113,7 @@ impl Game {
             }
             Direction::Down => {
                 position.y += 1;
-                if position.y >= self.field.height - 1{
+                if position.y >= self.field.height - 1 {
                     position.y = 0;
                 }
             }
@@ -124,7 +126,7 @@ impl Game {
             }
             Direction::Right => {
                 position.x += 1;
-                if position.x >= self.field.width - 1{
+                if position.x >= self.field.width - 1 {
                     position.x = 0;
                 }
             }
@@ -134,7 +136,7 @@ impl Game {
 
     fn display_field_once(&self) {
         let mut stdout = stdout();
-        queue!(stdout, Clear(ClearType::All), cursor::MoveTo(0,0)).unwrap();
+        queue!(stdout, Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
         for _ in 0..self.field.height {
             for _ in 0..self.field.width {
                 queue!(stdout, Print("X ")).unwrap();
@@ -148,24 +150,42 @@ impl Game {
     fn refresh_field(&self) {
         let mut stdout = stdout();
         let clients = self.clients.read();
+
+        // collect all positions
+        let mut map: HashMap<Position, Vec<&Client>> = HashMap::new();
         for client in clients.iter() {
             let position = client.position.read();
+            let entry = map.entry(position.clone()).or_insert(Vec::new());
+            entry.push(client);
+        }
+
+        // print all clients
+        for client in clients.iter() {
+            let position = client.position.read();
+            let old_position = client.old_position.read();
             queue!(
                 stdout,
-                cursor::MoveTo(position.x * 2 , position.y),
+                cursor::MoveTo(position.x * 2, position.y),
                 SetForegroundColor(Color::Red),
-                Print("X"),
+                Print(client.id.to_string()),
                 ResetColor
             )
             .unwrap();
+
             // restore old position if there is no client
-            
+            if map.get(&old_position).is_none() {
+                queue!(
+                    stdout,
+                    cursor::MoveTo(old_position.x * 2, old_position.y),
+                    Print("C")
+                )
+                .unwrap();
+            }
         }
         stdout.flush().unwrap();
     }
 
     pub async fn run(&self) {
-
         self.display_field_once();
         loop {
             tokio::time::sleep(Duration::from_secs(1)).await;
@@ -192,8 +212,10 @@ impl Game {
                     for client in clients.iter_mut() {
                         client.adjust_score(-5);
                         //move the client to a random position
-                        let position = self.create_random_position();
-                        client.set_position(position);
+                        let old_position = client.position.read().clone();
+                        let new_position = self.create_random_position();
+                        client.set_old_position(old_position);
+                        client.set_position(new_position);
                     }
                 }
             }
